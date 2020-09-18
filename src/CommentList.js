2074b49cf525cc525690655027370bf0f3c27657
RizwanSAA <sabaarizwan@outlook.com>
2020-09-18 22:41:03 +0530

    ready client for microservice

---
+++ src/CommentList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CommentList = ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
+    switch (comment.status) {
+      case "approved":
+        content = comment.content;
+        break;
+      case "rejected":
+        content = "This comment has been rejected";
+        break;
+      case "pending":
+        content = "This comment is awaiting moderation";
+        break;
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
